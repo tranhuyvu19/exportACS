f7029f6921d3d0c019a9b58695480822ad220e11
tranhuyvu19 <tranhuyvu19@gmail.com>
2016-02-04 16:27:21 +0700

    firt commit

--- tables.py
+++ tables.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 from sqlalchemy import BigInteger, Column, DateTime, ForeignKey, Index, Integer, String, Table, Text, text
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
